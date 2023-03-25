9664acb26dc0caa43cf7f62413586830f765df73
Nelson Amaya <nelsonamaya82@gmail.com>
2023-03-24 21:05:32 -0500

    Mobile Menu and Main Menu adjustments

--- asset/js/script.js
+++ asset/js/script.js
@@ -4,15 +4,22 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
     const mainHeader = document.querySelector('.main-header');
     const mainHeaderTopBar = document.querySelector('.main-header__top-bar');
+    const mainHeaderMainBar = document.querySelector('.main-header__main-bar');
+    const menuDrawer = document.getElementById('menu-drawer');
+
     let lastKnownScrollPosition = 0;
     let ticking = false;
     let scrollDirection = 'up';
 
+    onScroll();
+
     function onScroll(scrollPos) {
         if(scrollPos > 20 && scrollDirection == 'down') {
             mainHeader.style.top = - (mainHeaderTopBar.offsetHeight) + 'px';
+            menuDrawer.style.top = mainHeaderMainBar.offsetHeight + 'px';
         } else {
             mainHeader.style.top = 0;
+            menuDrawer.style.top = mainHeader.offsetHeight + 'px';
         }
     }
 
